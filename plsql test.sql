1cd08732a86f8c059de48c181df2965d9bcb2bf6
Subeeesh <64542922+Subeeesh@users.noreply.github.com>
2021-08-07 16:30:57 +0530

    Update plsql test.sql

--- plsql test.sql
+++ plsql test.sql
@@ -29,6 +29,50 @@ insert into empcommnul select * from myemp where comm is null;
 delete from myemp where comm is null;
 select * from myemp;
 
+select * from ebill;
+
+desc ebill;
+
+DELIMITER //
+create procedure cal_bamt(in x varchar(10), out o decimal(8,2))
+begin
+declare done boolean default 0;
+declare uits int(4);
+declare amt_cur cursor for
+select nounits from ebill where cno=x;
+open amt_cur;
+fetch amt_cur into uits;
+if uits>200 then
+set o=(uits-200)*1.5+150;
+elseif uits>100 and uits<200 then
+set o=(uits-100)*1.5+50;
+else
+set o=uits*0.5;
+end if;
+close amt_cur;
+update ebill set bamt=o where cno=x;
+select * from ebill where cno=x;
+end //
+
+
+DELIMITER //
+create trigger day_trig1 before insert on myemp for each row 
+begin
+declare val1 varchar(20);
+declare val2 varchar(20);
+declare val3 varchar(20);
+set val1='Saturday';
+set val2='Sunday';
+set val3=current_date();
+if strcmp(val1,dayname(val3))=0 || strcmp(val2,dayname(val3))=0 then
+SIGNAL SQLSTATE '50001' SET MESSAGE_TEXT = 'Cannot insert on week end days';
+end if;
+end //
+
+insert into myemp values(10,'jacksparrow',2157,23000,3,100,5);
+
+
+
 
 
 
